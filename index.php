6eca275600fe7af1da1afc74aa9932aab008f3b5
D24809 <darshanmarathe@gmail.com>
2017-05-03 19:39:45 +0530

    Added partial js support

--- index.php
+++ index.php
@@ -8,10 +8,15 @@
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     </head>
     <body>
+   
+   <partial src="/partials/header.php"></partial>
+   
     <h1><?php echo "Alma factor app"; ?> </h1>
     <hr>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+   
+   <script src="/js/partial.js" ></script>
     </body>
 </html>
