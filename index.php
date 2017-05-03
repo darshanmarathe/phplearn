e2e21e3b01074af1560705c995b5b2a5304b2c9b
D24809 <darshanmarathe@gmail.com>
2017-05-03 19:33:19 +0530

    Added bootstrap support

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/style.css" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     </head>
     <body>
-    
-    <?php
+    <h1><?php echo "Alma factor app"; ?> </h1>
+    <hr>
 
-    echo "Hello world";
-
-    ?>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
     </body>
 </html>
